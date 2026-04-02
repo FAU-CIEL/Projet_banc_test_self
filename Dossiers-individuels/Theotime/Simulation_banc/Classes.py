61fd8a9d9192faad902b4068a7d38e69153ee0d9
Theotime Faure <theotimefaure@free.fr>
2026-04-02 14:47:00 +0200

    modif scripts principaux

--- Dossiers-individuels/Theotime/Simulation_banc/Classes.py
+++ Dossiers-individuels/Theotime/Simulation_banc/Classes.py
@@ -8,20 +8,24 @@ import sys                          # classe systeme (environement + entrée/sor
 import io                           # classe flux entree/sortie
 import os                           # classe systeme (gestion fichiers)
 
+
 led_pret = machine.Pin(2, machine.Pin.OUT)
 
 # classe pour la reception des trames de commande/parametrage
 class Gestion_Reception:
-    def __init__(self):
+    def __init__(self) -> None:
         self.__trame = ""
         self.trame_correct = False
-        self.parametre_sim = []        
+        self.__liste_code_err = [1, 2, 3, 4, 5, 6, 7, 8, 9] # a revoir
+        self.__liste_commande = ["SET_CONF", "START", "STOP", "GET_MEAS", "GET_STATUS", "RESET", "MEAS", "ERR;", "recevoir\n"]
+        self.action = ""
+        self.parametre_sim = [0, 0]        
         """
-        Premier arg =>
-        Deuxieme arg =>
+        Premier arg => nb echantillon
+        Deuxieme arg => frequence d'echantillonage
         """
 
-    def reception_trame(self):
+    def reception_trame(self) -> None:
         self.__trame = ""
         if sys.stdin in select.select([sys.stdin], [], [], 0)[0]:
             self.__trame = sys.stdin.readline()
@@ -29,15 +33,29 @@ class Gestion_Reception:
                 f.write(self.__trame)
             f.close()
 
-    def decoupage_trame(self):
-        if self.__trame == "recevoir\n":
-            self.trame_correct = True
-        else:
-            self.trame_correct = False
+    def __chaine_presente(self, commande) -> bool:
+        return True if commande in self.__trame else False
+
+    def decoupage_trame(self) -> None:
+        if self.__chaine_presente(self.__liste_commande[0]): # SET_CONF
+            trame_split = self.__trame.split(";")
+            self.parametre_sim[0] = int(trame_split[1].split('=')[1])
+            self.parametre_sim[1] = int(trame_split[2].split('=')[1])
+
+    def action_trame(self) -> None:
+        for i in range(len(self.__liste_commande)):
+            if self.__chaine_presente(self.__liste_commande[i]):
+                self.trame_correct = True
+                self.action = self.__liste_commande[i]
+                self.decoupage_trame()
+                break
+            else:
+                self.trame_correct = False
+
 
 # classe qui simule le circuit (equa diff + transformer en Z)
 class Simulation:
-    def __init__(self):
+    def __init__(self) -> None:
         # parametre physique
         self.__L = 0
         self.__C = 0
@@ -47,7 +65,6 @@ class Simulation:
 
         # gestion du temps
         self.__dt = 0       # pas de temps
-        self.__duree = 0    # temps de sim
         self.__n_steps = 0  # nb d'etapes
 
         self.__omega0 = 0
@@ -58,13 +75,12 @@ class Simulation:
         self.u_l = []
         self.i_l = []
 
-    def init_parametre(self, L, C, U0, dt, duree):
+    def init_parametre(self, L, C, U0, dt, nb_step) -> None:
         self.__L = L
         self.__C = C
         self.__U0 = U0
         self.__dt = dt
-        self.__duree = duree
-        self.__n_steps = int(max(1, math.ceil(self.__duree / self.__dt)))
+        self.__n_steps = nb_step
         self.__omega0 = 1 / math.sqrt(self.__L * self.__C)
         self.__phi = 0
 
@@ -84,17 +100,15 @@ class Simulation:
     def __transformer_Z_intensite(self, z) -> float:
         return self.__C * self.__U0 * self.__omega0 * ((z * math.sin(self.__omega0)) / (z ** 2 -2 * z * math.cos(self.__omega0) + 1))
     
-    def simulation(self):
+    def simulation(self) -> None:
         for N in range(self.__n_steps):
             self.t.append(self.__dt * N)
             self.u_l.append(self.__transformer_Z_tension(self.t[N]))
             self.i_l.append(self.__transformer_Z_intensite(self.t[N]))
 
-
 # classe qui stocke les valeurs dans un json
-# classe terminer?
 class Gestion_json:
-    def __init__(self):
+    def __init__(self) -> None:
         self.__chemin_json = "LOG"
         self.__fichier_json = "donnees_self.json"
         self.__donner_a_stocker = {
@@ -117,29 +131,29 @@ class Gestion_json:
                 return True
         return False
     
-    def __creer_chemin_json(self):
+    def __creer_chemin_json(self) -> None:
         if not self.__dossier_existant():
             os.mkdir(self.__chemin_json)
     
-    def creer_json(self):
+    def creer_json(self) -> None:
         if sys.platform == "esp32":
             self.__creer_chemin_json()
             os.chdir(self.__chemin_json)
             io.open(self.__fichier_json, 'w')
 
-    def preparation_donnee(self, donnee, valeur):
+    def preparation_donnee(self, donnee, valeur) -> None:
         self.__donner_a_stocker[donnee] = valeur
     
-    def charger_json(self):
+    def charger_json(self) -> None:
         with io.open(self.__fichier_json, 't') as file:
             json.dump(self.__donner_a_stocker, file)
         file.close()
 
-    def __detruire_file(self):
+    def __detruire_file(self) -> None:
         os.chdir("/" + self.__chemin_json)
         os.remove(self.__fichier_json)
 
-    def detruire_json(self):
+    def detruire_json(self) -> None:
         if sys.platform == "esp32":
             os.chdir("/")
             if self.__dossier_existant():
@@ -150,7 +164,7 @@ class Gestion_json:
 
 # classe qui renvoie les données a l'IHM
 class Gestion_envoi:
-    def __init__(self):
+    def __init__(self) -> None:
         self.__chemin_fichier_json = "LOG"
         self.__fichier_json = "donnees_self.json"
 
@@ -173,7 +187,7 @@ class Gestion_envoi:
             else:
                 return False
 
-    def envoi_donnees(self):
+    def envoi_donnees(self) -> None:
         if self.__sortie_prete() and self.__fichier_existant():
             with io.open(self.__fichier_json, 'r') as file:
                 for line in file:
@@ -181,47 +195,86 @@ class Gestion_envoi:
 
 # classe qui gere les autres fonctions
 class Gestion_fonction:
-    def __init__(self):
+    def __init__(self) -> None:
         self.__reception = Gestion_Reception()
         self.__Simulation_banc = Simulation()
         self.__mon_json = Gestion_json()
         self.__retour_donnees = Gestion_envoi()
 
-    def loop(self):
+    def __prepa_json(self) -> None:
+        self.__mon_json.creer_json()
+        self.__mon_json.preparation_donnee("temps", self.__Simulation_banc.t)
+        self.__mon_json.preparation_donnee("tension",self.__Simulation_banc.u_l)
+        self.__mon_json.preparation_donnee("intensite", self.__Simulation_banc.i_l)
+        self.__mon_json.charger_json()
+        time.sleep(1)
+
+    def __envoi_et_preparation_futur_test(self) -> None:
+        self.__retour_donnees.envoi_donnees()
+        self.__reception.trame_correct = False
+        print("\n")
+        self.__mon_json.detruire_json()
+
+    def loop(self) -> None:
         led_pret.off()
         while True:
             led_pret.on()
             
-            self.__mon_json.detruire_json() # fonction a revoir
-            # ==============================
-            # Reception trame
-            # ==============================
             self.__reception.reception_trame()
-            self.__reception.decoupage_trame()
+            self.__reception.action_trame()
+
             if self.__reception.trame_correct:
 
-                led_pret.off()
+                if self.__reception.action == "SET_CONF":
+                    print("OK;CMD")
+                    led_pret.off()
+                    self.__Simulation_banc.init_parametre(L=0.330, 
+                                                          C=0.5, 
+                                                          U0=50, 
+                                                          dt=1/self.__reception.parametre_sim[1], 
+                                                          nb_step=self.__reception.parametre_sim[0])
+                    break
 
-                # ==============================
-                # Simulation
-                # ==============================
-                self.__Simulation_banc.init_parametre(L=0.330, C=0.5, U0=50, dt=1/10, duree=5)
-                self.__Simulation_banc.simulation()
+                if self.__reception.action == "START":
+                    print("OK;CMD")
+                    led_pret.off()
+                    self.__Simulation_banc.simulation()
+                    break
 
-                # ==============================
-                # Json
-                # ==============================
-                self.__mon_json.creer_json()
-                self.__mon_json.preparation_donnee("temps", self.__Simulation_banc.t)
-                self.__mon_json.preparation_donnee("tension",self.__Simulation_banc.u_l)
-                self.__mon_json.preparation_donnee("intensite", self.__Simulation_banc.i_l)
-                self.__mon_json.charger_json()
-                time.sleep(1)
+                if self.__reception.action == "STOP":
+                    print("OK;CMD")
+                    led_pret.off()
+                    break
+                
+                if self.__reception.action == "GET_MEAS":
+                    print("OK;CMD")
+                    led_pret.off()
+                    break
 
-                # ==============================
-                # envoie données + preparation futur sim
-                # ==============================
-                self.__retour_donnees.envoi_donnees()
-                self.__reception.trame_correct = False
-                print("\n")
-                break
+                if self.__reception.action == "GET_STATUS":
+                    print("OK;CMD")
+                    led_pret.off()
+                    print("simulation prete")
+                    break
+
+                if self.__reception.action == "RESET":
+                    print("OK;CMD")
+                    led_pret.off()
+                    machine.reset()
+                    break
+                    
+                if self.__reception.action == "MEAS":
+                    print("OK;CMD")
+                    led_pret.off()
+                    self.__prepa_json()
+                    self.__envoi_et_preparation_futur_test()
+                    break
+                
+                if self.__reception.action == "recevoir\n":
+                    print("OK;CMD")
+                    led_pret.off()
+                    self.__Simulation_banc.init_parametre(L=0.330, C=0.5, U0=50, dt=1/10, nb_step=50)
+                    self.__Simulation_banc.simulation()
+                    self.__prepa_json()
+                    self.__envoi_et_preparation_futur_test()
+                    break
--- Dossiers-individuels/Theotime/Simulation_banc/fonction_principal.py
+++ Dossiers-individuels/Theotime/Simulation_banc/fonction_principal.py
@@ -9,6 +9,7 @@ while True:
     #banc_de_self.Simulation_banc.u_l = [5.0,10.0,15.0,20.0,25.0]
     banc_de_self.loop()
 
-    time.sleep(10)
+    time.sleep(10) # evite le pipelining
+
     # permet de suprimer les données les objets python et variables de la memoire. permet d'eviter "memory allocation fail"
     machine.soft_reset()
